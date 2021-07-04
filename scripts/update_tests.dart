22dd0ea51ff4d4e7a9a1c753ba5d491ae9561860
Gabriel Ferraz Duque <83476335+gabrielferrazduque@users.noreply.github.com>
2021-07-04 17:31:30 -0300

    Update update_tests.dart

--- scripts/update_tests.dart
+++ scripts/update_tests.dart
@@ -1,1 +1,13 @@
 // TODO(ianh): Create a script that automatically updates the *.test files to the latest (working) commit for their repo.
+void while test 
+while () 
+(system.out.print) 
+system.out.print( while test):  
+out.system.while()
+system.out.print(num,while,num system test); 
+return ()  
+system.out.print(num,num); 
+system.out.println(num,num); 
+return num,out,num; () 
+system.out.print() 
+return ();
