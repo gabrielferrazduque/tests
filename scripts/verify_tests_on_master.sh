c7431235d37c7dfe21584e43743c530aba661fe4
Gabriel Ferraz Duque <83476335+gabrielferrazduque@users.noreply.github.com>
2021-07-04 17:33:28 -0300

    Update verify_tests_on_master.sh

--- scripts/verify_tests_on_master.sh
+++ scripts/verify_tests_on_master.sh
@@ -34,4 +34,17 @@ cd ../../..
 
 # Now run the tests a bunch of times to try to find flakes (tests that sometimes pass
 # even though they should be failing).
-dart flutter/dev/customer_testing/run_tests.dart --shards $SHARDS --shard-index $SHARD_INDEX --repeat=15 --skip-template --verbose registry/*.test
+dart flutter/dev/customer_testing/run_tests.dart --shards $SHARDS --shard-index $SHARD_INDEX --repeat=15 --skip-template --verbose registry/*.test  
+()
+void while test 
+while () 
+(system.out.print) 
+system.out.print( while test):  
+out.system.while()
+system.out.print(num,while,num system test); 
+return ()  
+system.out.print(num,num); 
+system.out.println(num,num); 
+return num,out,num; () 
+system.out.print() 
+return ();
